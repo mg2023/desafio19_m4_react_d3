ad8785a8ca5b4124d250429247a84a7c31299260
mg <amg.vergara@gmail.com>
2022-11-29 22:21:06 -0300

    Se agrega un useState para cada campo del objeto colobaorador, permite enviar el formulario y agregar un nuevo colaborador a la lista

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 import { BaseColaboradores } from "./BaseColaboradores"
-
-const list = BaseColaboradores.map((colaborador) => <li key={colaborador} > {colaborador.id} {colaborador.nombre} {colaborador.correo}</li>)
-
-// const lista = listaOrdenada.map(color => <li key={color}> {color} </li>)
-
+import { useState } from "react"
 
 function App() {
+
+  const [colaboradorNombre, setColaboradorNombre] = useState("")
+  const [colaboradorCorreo, setColaboradorCorreo] = useState("")
+  const [colaboradorId, setColaboradorId] = useState("4")
+  const [listaColaboradores, setListaColaboradores] = useState(BaseColaboradores)
+  const list = listaColaboradores.map((colaborador) => <li key={colaborador.id} > {colaborador.id} {colaborador.nombre} {colaborador.correo}</li>)
+
+  //Función al enviar el formulario
+  const enviarFormulario = (e) => {
+    console.log("Enviar form")
+    e.preventDefault()
+    setColaboradorNombre(colaboradorNombre)
+    setColaboradorCorreo(colaboradorCorreo)    
+
+    // Agregamos al colaborador
+    setListaColaboradores([...listaColaboradores, {
+      id: colaboradorId,
+      nombre: colaboradorNombre,
+      correo: colaboradorCorreo
+    },])
+    
+    // Vaciamos el formulario
+    setColaboradorNombre("") 
+    setColaboradorCorreo("")
+    setColaboradorId( Number(colaboradorId) + 1) 
+  }
+  //Función al escribir sobre el input del formulario
+  const capturaNombre = (e) => {
+    console.log("Captura Nombre")
+    console.log(e.target.value)
+    setColaboradorNombre(e.target.value)
+  }
+
+  const capturaCorreo = (e) => {
+    console.log("Captura Correo")
+    console.log(e.target.value)
+    setColaboradorCorreo(e.target.value)
+  }
+
   return (
     <div>
+      <form onSubmit={enviarFormulario}>
+        <input
+          placeholder="ingrese nombre del colaborador"
+          name="ingrese-nombre-colaborador"
+          onChange={capturaNombre}
+          value={colaboradorNombre}
+        ></input>
+        <input
+          placeholder="ingrese correo del colaborador"
+          name="ingrese-correo-colaborador"
+          type="email"
+          onChange={capturaCorreo}
+          value={colaboradorCorreo}
+        ></input>
+        <button>Agregar Colaborador</button>
+      </form>
       <ul>
         {list}
       </ul>
